fdf45aca7796867807e56a293d7cd2f16cdab62e
Eran Weiser <eran.weiser@gmail.com>
2022-08-27 08:08:19 +0300

    feat: Skeleton
    This branch includes empty table with interpolation of application header

---
+++ src/app/components/products/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "product-list",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductList {
+    
+    productTitle: string = "Products List";
+}
