3dcd559271a18ce7b615d14d9ca5055eb3c9135f
Eran Weiser <eran.weiser@gmail.com>
2022-08-28 17:36:37 +0300

    feat: creating a service

--- src/app/components/products/product-list.component.ts
+++ src/app/components/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "src/app/models/products";
+import { ProductService } from "src/app/services/product-list.service";
 
 @Component({
     selector: "product-list",
@@ -17,86 +18,43 @@ import { IProduct } from "src/app/models/products";
 export class ProductList implements OnInit{
     
     productTitle: string = "Products List";
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2021",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2021",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2021",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        },
-        {
-          "productId": 8,
-          "productName": "Saw",
-          "productCode": "TBX-0022",
-          "releaseDate": "May 15, 2021",
-          "description": "15-inch steel blade hand saw",
-          "price": 11.55,
-          "starRating": 3.7,
-          "imageUrl": "assets/images/saw.png"
-        },
-        {
-          "productId": 10,
-          "productName": "Video Game Controller",
-          "productCode": "GMG-0042",
-          "releaseDate": "October 15, 2020",
-          "description": "Standard two-button video game controller",
-          "price": 35.95,
-          "starRating": 4.6,
-          "imageUrl": "assets/images/xbox-controller.png"
-        }
-      ];
-      filteredProducts: IProduct[] = [];
-      imageWidth: number = 50;
-      imageMargin: number = 2;
-      showImage: boolean = false;
-      _listFilter: string = "";
+    products: IProduct[] = []
+    filteredProducts: IProduct[] = [];
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    _listFilter: string = "";
 
-      get listFilter() : string {
-        return this._listFilter;
-      }
+    /**
+     *
+     */
+    constructor(private productService: ProductService) {
+        
+    }
 
-      set listFilter(value: string) {
-        this._listFilter = value;
+    get listFilter() : string {
+      return this._listFilter;
+    }
+   
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.performFilter();
+    }
+   
+    ngOnInit() :void {
+        this.products = this.productService.getProducts();
         this.filteredProducts = this.performFilter();
-      }
-
-      ngOnInit() :void {
-        this.filteredProducts = this.performFilter();
-
-      }
-      toggleImage(): void {
+    }
+    
+    toggleImage(): void {
         this.showImage = !this.showImage;
-      }
-
-      performFilter() : IProduct[] {
-        return this.products.filter( (product: IProduct) => product.productName.toLowerCase().includes(this._listFilter.toLowerCase()) )
-      }
-
-      onRatingClicked(message: string) {
-        this.productTitle = `Products List: ${message}`
-      }
+    }
+   
+    performFilter() : IProduct[] {
+      return this.products.filter( (product: IProduct) => product.productName.toLowerCase().includes(this._listFilter.toLowerCase()) )
+    }
+   
+    onRatingClicked(message: string) {
+      this.productTitle = `Products List: ${message}`
+    }
 }
